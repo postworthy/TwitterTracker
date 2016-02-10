3a5b33f13977d34ac78c183a47769cbca228fc28
unknown <landon.key+pw@gmail.com>
2016-02-10 08:14:35 -0600

    mono compatibility update

--- TwitterTracker/Program.cs
+++ TwitterTracker/Program.cs
@@ -11,12 +11,21 @@ namespace TwitterTracker
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length < 2)
             {
                 Console.WriteLine("What would you like to track (ex: Twitter,Google,Apple)");
-                args = new[] { "-tv", Console.ReadLine() };
+                args = new[] { args.Length == 1 ? args[0] : "-tv", Console.ReadLine() };
             }
-            
+
+            //Bypass Cert Validation
+            if (args[0].Contains("x"))
+            {
+                System.Net.ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => {
+                    return true;
+                };
+            }
+
+
             var tracker = Tracker.New(
                 args[1], 
                 args[0].Contains("v") ? Console.Out : null);
