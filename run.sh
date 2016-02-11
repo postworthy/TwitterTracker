7619f18d1ae2b81c979617a14f6c1be4f20bd59f
Landon Key <landon.key+pw@gmail.com>
2016-02-10 18:54:20 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mono TwitterTracker/bin/Debug/TwitterTracker.exe -t \#news | mono TwitterTracker.Extract.Urls/bin/Debug/TwitterTracker.Extract.Urls.exe | mono TwitterTracker.Utilities.Frequency/bin/Debug/TwitterTracker.Utilities.Frequency.exe
+mono TwitterTracker/bin/Debug/TwitterTracker.exe -tx \#news | mono TwitterTracker.Extract.Urls/bin/Debug/TwitterTracker.Extract.Urls.exe | mono TwitterTracker.Utilities.Frequency/bin/Debug/TwitterTracker.Utilities.Frequency.exe -console
