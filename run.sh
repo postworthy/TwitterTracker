95886cdb9ab6b6090e7f540c49cdd583629c076b
Landon Key <landonkey@Joshuas-MBP.attlocal.net>
2016-02-12 18:31:49 -0600

    Cert Update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mono TwitterTracker/bin/Debug/TwitterTracker.exe -tx \#news | mono TwitterTracker.Extract.Urls/bin/Debug/TwitterTracker.Extract.Urls.exe | mono TwitterTracker.Utilities.Frequency/bin/Debug/TwitterTracker.Utilities.Frequency.exe | mono CloudUtility/bin/Debug/CloudUtility.exe -u
+mono TwitterTracker/bin/Debug/TwitterTracker.exe -tx \#news | mono TwitterTracker.Extract.Urls/bin/Debug/TwitterTracker.Extract.Urls.exe | mono TwitterTracker.Utilities.Frequency/bin/Debug/TwitterTracker.Utilities.Frequency.exe | mono CloudUtility/bin/Debug/CloudUtility.exe -ux
