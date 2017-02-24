138fc9b543167a38cc0fdd773d60e4e9367bf6ea
Landon Key <landon.key+pw@gmail.com>
2017-02-23 21:30:48 -0600

    HttpPostUtility
    HttpPostUtility is for piping in data to post to a URL

---
+++ HttpPostUtility/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpPostUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri destination = null;
+            string postParameterName = null;
+            if (args.Length == 2 && Uri.TryCreate(args[0], UriKind.Absolute, out destination))
+            {
+                postParameterName = args[1];
+                var input = Console.ReadLine();
+                while (!string.IsNullOrEmpty(input))
+                {
+                    Task.Factory.StartNew(() =>
+                    {
+                        try
+                        {
+                            using (WebClient client = new WebClient())
+                            {
+                                byte[] response =
+                                client.UploadValues(destination, new NameValueCollection()
+                                {
+                                    { postParameterName, input }
+                                });
+                            }
+                        }
+                        catch { }
+                    });
+                    input = Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage: HttpPostUtility <URL_TO_POST_DATA> <POST_PARAMETER_NAME>");
+                Console.WriteLine("EX: HttpPostUtility http://yourawesomedomainname.tld/api/endpoint postdata");
+            }
+        }
+    }
+}
