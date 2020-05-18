9296166eeb888e9877af27b80822e2cfc57aa778
Landon Key <>
2020-05-18 15:18:08 -0500

    Updated examples of how to pipe commands together

--- run.ps1
+++ run.ps1
@@ -1,3 +1,8 @@
 #dotnet TwitterTracker\bin\Debug\netcoreapp2.1\TwitterTracker.dll -tx news | ForEach-Object { [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($_)) | ConvertFrom-Json }
 $track="CVE-"
 dotnet TwitterTracker\bin\Debug\netcoreapp2.1\TwitterTracker.dll -tx $track | ForEach-Object { [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($_)) | ConvertFrom-Json } | select text
+
+#dotnet TwitterTracker\bin\Debug\netcoreapp2.1\TwitterTracker.dll -tx github | ForEach-Object { $_ | dotnet TwitterTracker.Extract.Urls\bin\Debug\netcoreapp2.1\TwitterTracker.Extract.Urls.dll }
+
+#Also from powershell you can execute the pipes like this:
+#cmd /C 'dotnet TwitterTracker\bin\Debug\netcoreapp2.1\TwitterTracker.dll -tx news | dotnet TwitterTracker.Filter\bin\Debug\netcoreapp2.1\TwitterTracker.Filter.dll "$.root[?(@.user.followers_count>400)]" | dotnet TwitterTracker.Extract.Urls\bin\Debug\netcoreapp2.1\TwitterTracker.Extract.Urls.dll''
