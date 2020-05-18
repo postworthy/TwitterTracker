b2ee7fb59d37e95ad05e61d61b58ad95ed5fc41a
Landon Key <>
2020-05-18 15:31:04 -0500

    Examples added for ranking urls passing through the stream

--- run.ps1
+++ run.ps1
@@ -6,3 +6,6 @@ dotnet TwitterTracker\bin\Debug\netcoreapp2.1\TwitterTracker.dll -tx $track | Fo
 
 #Also from powershell you can execute the pipes like this:
 #cmd /C 'dotnet TwitterTracker\bin\Debug\netcoreapp2.1\TwitterTracker.dll -tx news | dotnet TwitterTracker.Filter\bin\Debug\netcoreapp2.1\TwitterTracker.Filter.dll "$.root[?(@.user.followers_count>400)]" | dotnet TwitterTracker.Extract.Urls\bin\Debug\netcoreapp2.1\TwitterTracker.Extract.Urls.dll''
+
+#This example will begin building as list of the top 50 most populat URLs that pass through the stream
+#cmd /C 'dotnet TwitterTracker\bin\Debug\netcoreapp2.1\TwitterTracker.dll -tx news | dotnet TwitterTracker.Filter\bin\Debug\netcoreapp2.1\TwitterTracker.Filter.dll "$.root[?(@.user.followers_count>400)]" | dotnet TwitterTracker.Extract.Urls\bin\Debug\netcoreapp2.1\TwitterTracker.Extract.Urls.dll | dotnet TwitterTracker.Utilities.Frequency\bin\Debug\netcoreapp2.1\TwitterTracker.Utilities.Frequency.dll -console'
