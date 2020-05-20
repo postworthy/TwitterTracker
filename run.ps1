0cf6ff96ae61a578525b4cc4193c613b8f509aa0
Landon Key <>
2020-05-20 16:01:42 -0500

    TelegramBot examples added

--- run.ps1
+++ run.ps1
@@ -9,3 +9,6 @@ dotnet TwitterTracker\bin\Debug\netcoreapp2.1\TwitterTracker.dll -tx $track | Fo
 
 #This example will begin building as list of the top 50 most populat URLs that pass through the stream
 #cmd /C 'dotnet TwitterTracker\bin\Debug\netcoreapp2.1\TwitterTracker.dll -tx news | dotnet TwitterTracker.Filter\bin\Debug\netcoreapp2.1\TwitterTracker.Filter.dll "$.root[?(@.user.followers_count>400)]" | dotnet TwitterTracker.Extract.Urls\bin\Debug\netcoreapp2.1\TwitterTracker.Extract.Urls.dll | dotnet TwitterTracker.Utilities.Frequency\bin\Debug\netcoreapp2.1\TwitterTracker.Utilities.Frequency.dll -console'
+
+#This is an example of how to run the telegram bot for distribution of information
+#cmd /C 'dotnet TwitterTracker\bin\Debug\netcoreapp2.1\TwitterTracker.dll -tx "news" | dotnet TwitterTracker.Filter\bin\Debug\netcoreapp2.1\TwitterTracker.Filter.dll "$.root[?(@.user.followers_count>250)]" | dotnet TwitterTracker.Extract.Urls\bin\Debug\netcoreapp2.1\TwitterTracker.Extract.Urls.dll | dotnet TelegramBot\bin\Debug\netcoreapp3.1\TelegramBot.dll'
